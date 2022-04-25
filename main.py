9b83117b7d1593cc358b3b524e544c2b60c01c36
comeguillier <come.guillier@epita.fr>
2022-04-25 23:36:27 +0200

    Rendu Final

--- main.py
+++ main.py
@@ -9,12 +9,29 @@ Output :
 boolean : whether or not there exist two elements in the_array whose sum is exactly the_sum_value.
 
 """
+def sum_array(the_array):
+    # Creation of a new list with the sum of all the elements of the list
+    A = []
+    len_array = len(the_array)
+    B = the_array
+    sum = 0
+    j = 0
+    while j <= len_array -1:
+        i = j + 1
+        while i <= len_array - 1:
+            sum = B[j] + B[i]
+            A.append(sum)
+            i = i + 1
+        j = j + 1
+    return A
+
 def check_for_sum(the_array, the_sum_value):
-    
-    ##### ------ YOUR CODE ------ #####
+    # Check if the element is in the list
+    A = sum_array(the_array)
+    for i in range(len(A)):
+        if (A[i] == the_sum_value):
+            return True
     return False
-
-
 """
 MAIN
 
